4917ff0d669233879261aa7e3403e750cf8d2c95
Anthony <tonynarciso2003@gmail.com>
2026-09-04 16:13:34 -0500

    tarea 2 finalizada

--- clase 1/tarea2_1.py
+++ clase 1/tarea2_1.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+
 #parametros a utilizar
 a = 2.0
 N = 100
@@ -9,13 +11,20 @@ max_interaciones = 100
 #usando diccionario para organizar los puntos a graficar
 
 malla_puntos = {
-    "validos"
+    "validos":{
+        "x": [],
+        "y": []
+    },
+    "no_validos":{
+        "x": [],
+        "y": []
+    }
 }
 
 paso = (a - (-a))/(N-1)
 
 def Z(z:complex, c:complex):
-    z_actual = ((abs(z))**2) + c
+    z_actual = (z**2) + c
     return z_actual
 
 for i in range(N):
@@ -31,4 +40,22 @@ for x in eje_x:
         
         for interaciones in range(max_interaciones):
             z_actual = Z(z_actual, c)
+            if abs(z_actual) > 2:
+                pertenece_al_conjunto = False
+                break
+        
+        if pertenece_al_conjunto:
+            malla_puntos["validos"]["x"].append(x)
+            malla_puntos["validos"]["y"].append(y)
+        else:
+            malla_puntos["no_validos"]["x"].append(x)
+            malla_puntos["no_validos"]["y"].append(y)
+            
+plt.figure(figsize=(8, 8))
+plt.scatter(malla_puntos["no_validos"]["x"], malla_puntos["no_validos"]["y"], s=1, color="white")
+plt.scatter(malla_puntos["validos"]["x"], malla_puntos["validos"]["y"], s=1, color="black")
+plt.title("Visualización del Conjunto de Mandelbrot")
+plt.xlabel("Parte Real (x)")
+plt.ylabel("Parte Imaginaria (y)")
+plt.show()
             
